69c5af8d0e3a5ac70a4b3f0d0936b267d0bad65e
gildedguy9 <luisespinoza@liceovvh.cl>
2024-03-22 11:01:52 -0300

    ok

--- Fundamentos_JS/Cosas_Nuevas/scriptt.js
+++ Fundamentos_JS/Cosas_Nuevas/scriptt.js
@@ -1,42 +1,4 @@
-// Ejercicio 1: Declaración de variables
-
-
-function MiEdad() {
-    let Ahora = 2024;
-    let FechaNac = 2005;
-    let Resultado = Ahora - FechaNac;
-    alert(`Mi edad es de: ${Resultado}`);
-    
-    
-    
-    }
-    
-    // Ejercicio 2: Asignación de valores
-    
-    function AsignarValores() {
-    let Var1 = 37;
-    let Var2 = Var1;
-    alert(`El valor de Var2 es: ${Var2}`);
-    
-    
-    }
-    
-    // Ejercicio 3: Operaciones matemáticas
-    
-    function Operaciones() {
-    let Num2 = 745;
-    let Num1 = 180;
-    let Num3 = 45;
-    let Suma = Num1 + Num2 + Num3;
-    let Resta = Num1 - Num3;
-    let Multi = Num2 * Num3;
-    let Divi = Num1 / 120;
-    
-    alert(`El valor de Suma es ${Suma}\nEl valor de la Resta es ${Resta}\nEl valor de la Multi es  ${Multi}\nEl valor de la Division es ${Divi}`);
-    
-    }
-    
-    // Ejercicio 4: Manipulación de cadenas
+// Ejercicio 4: Manipulación de cadenas
     
     function MiDireccion() {
     let Direccion = "Avenida Sangregorio, La Granja";
